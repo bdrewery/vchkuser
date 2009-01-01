ef933c878c4567bd57f9078d8eb98b3fd11f7ac0
Benedikt Böhm <bb@xnull.de>
2009-01-01 23:13:40 +0100

    fix wrapper, remove gpl v3

--- vchkuser.c
+++ vchkuser.c
@@ -1,22 +1,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#ifndef VPOPMAILDIR
-#define VPOPMAILDIR "/var/vpopmail"
-#endif
-
-#ifndef VPOPMAILUID
-#define VPOPMAILUID 89
-#endif
-
-#ifndef VPOPMAILGID
-#define VPOPMAILGID 89
+#ifndef QMAILDIR
+#define QMAILDIR "/var/qmail"
 #endif
 
 int main(int argc, char *argv[])
 {
-	const char *vchkuser = VPOPMAILDIR "/plugins/vchkuser.sh";
-	setuid(VPOPMAILUID);
+	const char *vchkuser = QMAILDIR "/plugins/vchkuser.sh";
+	setresuid(0, 0, 0);
+	setresgid(0, 0, 0);
 	execl(vchkuser, vchkuser, NULL);
 	exit(EXIT_FAILURE);
 }
